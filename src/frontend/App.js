0db29bd84667bf04b601a434995bc03920e80361
Kamila <kamila.m.lewicka@gmail.com>
2020-02-07 14:00:02 +0000

    Results table created and styled

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import Search from './Search';
 import Calendar from './Calendar';
+import Results from './Results';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <body>
-        <Search search='language'/>
-        <Search search='location'/>
+        <Search search='language' />
+        <Search search='location' />
         <button className='searchbtn'>Search</button>
         <Calendar />
       </body>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Header} />
+          <Route exact path='/results' component={Results} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/frontend/Results.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Results.css';
+
+
+
+export default function Results() {
+        return (
+            <table className='resultsTable'>
+                <tr>
+                    <th>User</th>
+                    <th>Programming Language</th>
+                    <th>Location</th>
+                </tr>
+                <tr>
+                    <td>dummy Name</td>
+                    <td>dummy Language</td>
+                    <td>dummy Location</td>
+                </tr>
+            </table>
+        )
+}
