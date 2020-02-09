77b37332a16932303d57cfd3b30b41f12cf2b72a
Kamila <kamila.m.lewicka@gmail.com>
2020-02-09 21:56:32 +0000

    Results and Profile added to App

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -3,14 +3,21 @@ import Header from './Header';
 import Search from './Search';
 import Calendar from './Calendar';
 import Results from './Results';
+import Profile from './Profile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <body>
-        <Results />
-      </body>
+    <Header />
+    <body>
+      <Search search='language'/>
+      <Search search='location'/>
+      <button className='searchbtn'>Search</button>
+      <Calendar />
+      <Results />
+      <Profile />
+    </body>
     </div>
   );
 }
