98cb2e744812a10d23a70c7a51fef720308f8e0b
Kamila <kamila.m.lewicka@gmail.com>
2020-02-10 11:49:47 +0000

    removing import lines from app.js

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from './Header';
 import Search from './components/SearchBar/SearchBar';
 import Calendar from './Calendar';
-import Results from './Results';
-import Profile from './Profile';
 import './App.css';
 
 export default function App() {
