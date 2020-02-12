d3381983732e71f39a5e90d5f84c4e9b3218b92f
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-12 15:36:47 +0000

    accepting the comment

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Results from './components/Result/Results';
 import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
 import RoutingPage from './components/RoutingPage/RoutingPage';
 import './App.css';
 
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <div className="App">
     <Router>
+      <Header/>
       <Switch>
         <Route exact path="/" component={RoutingPage} />
         <Route exact path="/results" component={Results} /> 
