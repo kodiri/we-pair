7c4dd662fbf4282022285989130672e7e701640f
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-12 15:17:41 +0000

    delete an empty line\\

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Results from './components/Result/Results';
 import Profile from './components/Profile/Profile';
