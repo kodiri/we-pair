5474ff7c9bfbfbb583944d5df3c0a7aa4c5af03e
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-17 15:33:37 +0000

    would this work?

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
+// app.get('/', (req, res) => res.send());
+app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
-app.get('/', (req, res) => res.send());
-app.use(express.static(path.join(__dirname, '../../build')));
 app.listen(port, () => console.log(`example app listening on port ${port}`));
