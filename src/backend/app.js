3c85d8a6e0761b79a74415c6d341c3dfc2ac48e5
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-17 14:54:45 +0000

    some changes

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.send());
-
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 app.listen(port, () => console.log(`example app listening on port ${port}`));
