ca040abad1464b2021a126a32a40cd5a995730f2
Kamila <kamila.m.lewicka@gmail.com>
2020-02-24 16:49:00 +0000

    #37 Link to profile page when clicking on the profile photo in results component

---
+++ src/frontend/components/users.js
@@ -0,0 +1,36 @@
+const database = [
+  
+        {
+            id: '1',
+            name: 'Anitha Dharneedharan ',
+            Language: 'JavaScript',
+            Location: 'Sutton'
+        },
+        {
+            id: '2',
+            name: 'Florin Dumitru',
+            Language: 'Python',
+            Location: 'Walthamstow'
+        },
+        {
+            id: '3',
+            name: 'Kainy Ryu',
+            Language: 'Java',
+            Location: 'Bethnal Green'
+        },
+        {
+            id: '4',
+            name: 'Kamila Lew',
+            Language: 'C++',
+            Location: 'Heathrow'
+        },
+        {
+            id: '5',
+            name: 'Selchuk Karakus',
+            Language: 'Ruby',
+            Location: 'Leytonstone'
+        }
+    
+    ]
+
+    export default database;
