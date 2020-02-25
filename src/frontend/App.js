5bef3e20a63f3ae966f822651953bb086bf2d32f
Anitha Dharaneedharan <anitha.dharaneedharan@whitbread.com>
2020-02-25 22:39:40 +0000

    props drilling

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,22 +1,60 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Results from './components/Result/Results';
+import Results  from './components/Results/Results';
 import Profile from './components/Profile/Profile';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <Router>
-      <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/results" component={Results} />
-          <Route exact path="/profile" component={Profile} />
-        </Switch>
-      </Router>
-    </div>
-  );
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {filteredUsers: '' , filteredLocation: '' }
+  }
+
+  searchData(filteredUsers, filteredLocation,languageSearchQuery,locationSearchQuery ) {
+    
+    debugger;
+    if (languageSearchQuery && locationSearchQuery) {
+      this.setState({
+        filteredUsers,
+        filteredLocation
+      });
+    } else if (languageSearchQuery) {
+      this.setState({
+        filteredUsers
+      });
+    } else if (locationSearchQuery) {
+      this.setState({
+        filteredLocation
+      });
+    } else {
+      return "Please try again";
+    }
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+        <Header />
+          <Switch>
+            <Route exact path="/" render = {() => <Home searchData = {(a,b,c,d) => this.searchData(a,b,c,d)} />} />
+            <Route exact path="/results" component={Results} />
+            {/* <Route exact path="/profile" component={Profile} /> */}
+            <Route exact path="/profile/:id" component={Profile}></Route>
+            
+          </Switch>
+
+     
+        </Router>
+
+        
+      </div>
+    );
+
+  }
+
 }
--- src/frontend/components/SearchBar/SearchBar.js
+++ src/frontend/components/SearchBar/SearchBar.js
@@ -1,48 +1,106 @@
-import React from 'react';
-//import '../App.css';
-import './SearchBar.css';
+import React from "react";
+import "./SearchBar.css";
+// import { Link } from "react-router-dom";
+import SearchResults from "../Results/SearchResults";
+import users from "../getUsers"
 
-// Start array of user objects
-// const users = [
-//   {
-//     id: 1,
-//     name: 'Anitha Dharneedharan ',
-//     Language: 'JavaScript',
-//     Location: 'Sutton'
-//   },
-//   {
-//     id: 2,
-//     name: 'Florin Dumitru',
-//     Language: 'Python',
-//     Location: 'Walthamstow'
-//   },
-//   {
-//     id: 3,
-//     name: 'Kainy Ryu',
-//     Language: 'Java',
-//     Location: 'Bethnal Green'
-//   },
-//   {
-//     id: 4,
-//     name: 'Kamila Lew',
-//     Language: 'C++',
-//     Location: 'Heathrow'
-//   },
-//   {
-//     id: 5,
-//     name: 'Selchuk Karakus',
-//     Language: 'Ruby',
-//     Location: 'Leytonstone'
-//   }
-// ];
+import { Redirect } from "react-router";
 
-// End of user objects
 
-export default function SearchBar() {
-  return (
-    <div className="Search">
-      <input className="Search-language-input search-bar" type="search" placeholder="search language"/>
-      <input className="Search-location-input search-bar" type="search" placeholder="search location"/>
-    </div>
-  );
+export default class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      languageSearchQuery: "",
+      locationSearchQuery: "",
+      filteredUsers: "",
+      filteredLocation: "",
+      showResults: false
+    };
+  }
+
+  searchDatabase() {
+
+    
+    let filteredUsers = users
+      .filter(user =>
+        user.language
+          .toLowerCase()
+          .includes(this.state.languageSearchQuery.toLowerCase())
+      )
+      .map(profile => (
+        <SearchResults
+          key={profile.id}
+          id={profile.id}
+          name={profile.name}
+          language={profile.language}
+          location={profile.location}
+          availability={profile.availability}
+        />
+      ));
+    let filteredLocation = users
+      .filter(user =>
+        user.location
+          .toLowerCase()
+          .includes(this.state.locationSearchQuery.toLowerCase())
+      )
+      .map(profile => (
+        <SearchResults
+          key={profile.id}
+          id={profile.id}
+          name={profile.name}
+          language={profile.language}
+          location={profile.location}
+          availability={profile.availability}
+        />
+
+      ));
+    
+      this.props.searchData(filteredUsers, filteredLocation, this.state.languageSearchQuery,this.state.locationSearchQuery);
+
+      this.setState({showResults: true});
+
+
+    // if (this.state.languageSearchQuery && this.state.locationSearchQuery) {
+    //   this.setState({
+    //     filteredUsers,
+    //     filteredLocation
+    //   });
+    // } else if (this.state.languageSearchQuery) {
+    //   this.setState({
+    //     filteredUsers
+    //   });
+    // } else if (this.state.locationSearchQuery) {
+    //   this.setState({
+    //     filteredLocation
+    //   });
+    // } else {
+    //   return "Please try again";
+    // }
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input
+          className="search-language-input"
+          type="search"
+          placeholder="search language"
+          onChange={e => this.setState({ languageSearchQuery: e.target.value })}
+        />
+        <input
+          className="search-location-input"
+          type="search"
+          placeholder="search location"
+          onChange={e => this.setState({ locationSearchQuery: e.target.value })}
+        />
+        <button onClick={() => this.searchDatabase()}>Search</button>
+       {this.state.showResults && 
+         <Redirect to = "/results" />
+       }
+       
+       
+      </div>
+    );
+  }
 }
