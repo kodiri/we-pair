f68a4f960cb9c50cf92b03fa64669a88468ff2c7
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-27 21:23:09 +0000

    profile page changes for mobile

--- src/frontend/components/Profile/Profile.js
+++ src/frontend/components/Profile/Profile.js
@@ -21,31 +21,37 @@ export default class Profile extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="profile">
         <div className="user-wrapper">
           <img
             className="profile-image"
             src={this.state.userId.image}
             alt={this.state.userId.name}
           />
-          <div className="user-list user-name">{this.state.userId.name}</div>
+          <div className="profile-detail">
+            <span>&#123;</span>
+            <div className="user-list user-name"><span className="key">name: </span> <span className="value">	&#34;{this.state.userId.name}	&#34;</span></div>
 
-          <div className="user-list">
-            Likes to pair program in {this.state.userId.language}
-          </div>
+            <div className="user-list">
+            <span className="key">language: </span> <span className="value">	&#34;{this.state.userId.language}	&#34;</span>
+            </div>
 
-          <div className="user-list">
-            Location : {this.state.userId.location}
+            <div className="user-list">
+            <span className="key">location :</span> <span className="value">	&#34;{this.state.userId.location}&#34;</span>
+            </div>
+            <span>&#125;</span>
           </div>
+        </div>
+
 
           {/* <button className="send-button">Send</button> */}
           <PickDates/>
-          
-          <a href="mailto:anitha.k.dharaneedharan@gmail.com?Subject=Hello">
-            Send email
-          </a>
-         
-        </div>
+          <textarea placeholder="Leave a message..."/>
+          <div>
+            <a href="mailto:anitha.k.dharaneedharan@gmail.com?Subject=Hello">
+              Send email
+            </a>
+          </div>
       </div>
     );
   }
