c460f0cc1837da35b8647d749479bd4dab363029
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-27 22:11:36 +0000

    some css changes

--- src/frontend/components/Results/SearchResults.js
+++ src/frontend/components/Results/SearchResults.js
@@ -16,11 +16,14 @@ export default class SearchResults extends React.Component {
             <img className="user-image" src={this.props.image} alt="people" />
           </Link>
           <div className="stacked">
-          <h3>{this.props.name}</h3>
-            Would like to get hands dirty on{" "}
-            <strong>{this.props.language}</strong> at{" "}
-            <strong>{this.props.location}</strong> on{" "}
-            <strong>{this.props.availability}</strong>
+            <p className="result-detail">
+              &#123;<br />
+              <span className="key">name: </span><span className="value">{this.props.name}</span><br />
+              <span className="key">language: </span><span className="value">{this.props.language}</span><br />
+              <span className="key">location: </span><span className="value">{this.props.location}</span><br />
+              <span className="key">availability: </span><span className="value">{this.props.availability}</span><br />
+              &#125;
+            </p>
             {/* <Link to={`/profile/${this.props.id}`}> <span>See other availabale dates</span></Link> */}
           </div>
         </li>
