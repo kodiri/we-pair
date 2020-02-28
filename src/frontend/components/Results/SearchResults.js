fd06bb70c9e3a9bf7af026853a4a338b31f101f2
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-28 00:10:42 +0000

    some changes

--- src/frontend/components/Results/SearchResults.js
+++ src/frontend/components/Results/SearchResults.js
@@ -11,24 +11,35 @@ export default class SearchResults extends React.Component {
   render() {
     return (
       <>
-        <ul className="card">
-          <li className="card-list stack">
-            <Link to={`/profile/${this.props.id}`}>
+        <Link to={`/profile/${this.props.id}`}>
+          <ul className="card">
+            <li className="card-list stack">
               <img className="user-image" src={this.props.image} alt="people" />
-            </Link>
-            <div className="stacked">
-              <p className="result-detail">
-                &#123;<br />
-                <span className="key">name: </span><span className="value">&#34;{this.props.name}&#34;</span>,<br />
-                <span className="key">language: </span><span className="value">&#34;{this.props.language}&#34;</span>,<br />
-                <span className="key">location: </span><span className="value">&#34;{this.props.location}&#34;</span>,<br />
-                <span className="key">availability: </span><span className="value">&#34;{this.props.availability}&#34;</span><br />
-                &#125;
-              </p>
-              {/* <Link to={`/profile/${this.props.id}`}> <span>See other availabale dates</span></Link> */}
-            </div>
-          </li>
-        </ul>
+              <div className="stacked">
+                <p className="result-detail">
+                  &#123;
+                  <br />
+                  <span className="key">name: </span>
+                  <span className="value">&#34;{this.props.name}&#34;</span>,
+                  <br />
+                  <span className="key">language: </span>
+                  <span className="value">&#34;{this.props.language}&#34;</span>
+                  ,<br />
+                  <span className="key">location: </span>
+                  <span className="value">&#34;{this.props.location}&#34;</span>
+                  ,<br />
+                  <span className="key">availability: </span>
+                  <span className="value">
+                    &#34;{this.props.availability}&#34;
+                  </span>
+                  <br />
+                  &#125;
+                </p>
+                {/* <Link to={`/profile/${this.props.id}`}> <span>See other availabale dates</span></Link> */}
+              </div>
+            </li>
+          </ul>
+        </Link>
       </>
     );
   }
